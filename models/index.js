acded3c44c0330dad0612a1b17df018611091cbf
dan-cl <djclissold@yahoo.co.uk>
2016-08-16 17:31:28 +0100

    Adds clickable notes

--- models/index.js
+++ models/index.js
@@ -8,13 +8,33 @@ function createMessageClick(){
 
 var stringToObject = JSON.parse(localStorage.getItem("notes"));
 
-  if(localStorage.getItem("notes") !== null) {
-   window.onload = function() {
+if(localStorage.getItem("notes") !== null) {
+  window.onload = function() {
 
-   for(var i = 0; i < stringToObject.length;i++) {
+     for(var i = 0; i < stringToObject.length;i++) {
 
-   var messageList = document.getElementById("messages").innerHTML;
-   document.getElementById("messages").innerHTML = messageList + "<br>" + stringToObject[i]['message'];
+      var messageList = document.getElementById("messages").innerHTML;
+         document.getElementById("messages").innerHTML = messageList + "<br>" + "<a href=#" + stringToObject[i]['message'].replace(/ /g,"_") +">" + stringToObject[i]['message'].substring(0,20) + "...</a>";
+    }
+
+   };
 }
-  };
-}
+
+changeShowClickNoteForCurrentPage();
+
+function changeShowClickNoteForCurrentPage(){
+  window.addEventListener("hashchange", showClickNoteForCurrentPage);
+};
+
+function showClickNoteForCurrentPage(){
+showNote(getNoteFromURL(window.location));
+};
+
+function getNoteFromURL(location){
+  location.hash = location.hash.replace(/_/g," ");
+  return location.hash.split("#")[1];
+};
+
+function showNote(note){
+  document.getElementById("currentMessage").innerHTML = note;
+};
